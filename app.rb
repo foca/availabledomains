bb576273e8b36e0fc2688452ac5ff8c6e826b054
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2012-10-11 20:44:25 -0200

    Use Classy Sinatra, not Classic Sinatra

--- app.rb
+++ app.rb
@@ -1,15 +1,20 @@
 require "sinatra"
 require "whois"
 
-helpers do
+class AvailableDomains < Sinatra::Application
+  get "/" do
+    @results = query_glob(params[:e])
+    erb :home
+  end
+
+  # Check multiple domains for availability.
+  #
+  # expression - A String with a shell glob expression to expand.
+  #
+  # Returns an Array of available domain names.
   def query_glob(expression)
     expression = URI.decode(expression.to_s)
     domains = expression.empty? ? [] : `bash -c 'echo #{expression}'`.scan(/\S+/)
     domains.select { |domain| Whois.available?(domain) }
   end
-end
-
-get "/" do
-  @results = query_glob(params[:e])
-  erb :home
 end
