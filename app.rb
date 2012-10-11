194bf1ae83fef3739082a08e4643c9166231ec81
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2012-10-11 20:29:50 -0200

    Clean up the route itself

--- app.rb
+++ app.rb
@@ -2,13 +2,14 @@ require "sinatra"
 require "whois"
 
 helpers do
-  def expand(expression)
-    expression.empty? ? [] : `bash -c 'echo #{expression}'`.scan(/\S+/)
+  def query_glob(expression)
+    expression = URI.decode(expression.to_s)
+    domains = expression.empty? ? [] : `bash -c 'echo #{expression}'`.scan(/\S+/)
+    domains.select { |domain| Whois.available?(domain) }
   end
 end
 
 get "/" do
-  expression = URI.decode(params[:e].to_s)
-  @results = expand(expression).select { |domain| Whois.available?(domain) }
+  @results = query_glob(params[:e])
   erb :home
 end
